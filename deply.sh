de3024b5b53b093b0b22d48b0cc14803e70233a2
dushyant <{your_email}>
2026-09-21 19:35:57 +0530

    chaneg  the  root  dir name

--- deply.sh
+++ deply.sh
@@ -1,2 +1,4 @@
 git  pull
 npm run test 
+npm  run  build
+aws s3 cp dist/ s3://dushyant-storageapp-fronted --recursive
--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -26,7 +26,9 @@ function DirectoryView() {
   const { dirId } = useParams();
   const navigate = useNavigate();
 
-  const [directoryName, setDirectoryName] = useState("My StrorageApp");
+  const [directoryName, setDirectoryName] = useState(
+    "My StrorageApp Directory",
+  );
   const [directoriesList, setDirectoriesList] = useState([]);
   const [filesList, setFilesList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
@@ -53,7 +55,7 @@ function DirectoryView() {
   const loadDirectory = async () => {
     try {
       const data = await getDirectoryItems(dirId);
-      setDirectoryName(dirId ? data.name : "My StroargeApp");
+      setDirectoryName(dirId ? data.name : "My StrorageApp Directory");
       setDirectoriesList([...data.directories].reverse());
       setFilesList([...data.files].reverse());
     } catch (err) {
