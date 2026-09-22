8aa1752a445cc80e4fd5dddd6b4c10125db01411
dushyant <{your_email}>
2026-09-22 16:35:27 +0530

    change the filename

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -26,9 +26,7 @@ function DirectoryView() {
   const { dirId } = useParams();
   const navigate = useNavigate();
 
-  const [directoryName, setDirectoryName] = useState(
-    "My StrorageApp Directory view ",
-  );
+  const [directoryName, setDirectoryName] = useState("My StrorageApp Google");
   const [directoriesList, setDirectoriesList] = useState([]);
   const [filesList, setFilesList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
@@ -55,7 +53,7 @@ function DirectoryView() {
   const loadDirectory = async () => {
     try {
       const data = await getDirectoryItems(dirId);
-      setDirectoryName(dirId ? data.name : "My StrorageApp Directory view");
+      setDirectoryName(dirId ? data.name : "My StrorageApp Google");
       setDirectoriesList([...data.directories].reverse());
       setFilesList([...data.files].reverse());
     } catch (err) {
